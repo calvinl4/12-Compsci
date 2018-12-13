a9a947fd851680a3e79b842e8079cad50d98f015
calvinl4 <calvinlin@live.ca>
2018-12-13 10:51:45 -0500

    added numPassing method. Finished project.

--- LinearSearchGradebook/src/GradeBook.java
+++ LinearSearchGradebook/src/GradeBook.java
@@ -4,10 +4,11 @@ public class GradeBook {
 	public static void main (String[] args) {
 		int[] arr = {9, 5, 4, 3, 7};
 		System.out.println(gradeTotal(arr));
-		int[][] arr2d = {{10, 6, 9}, {9, 2, 10}, {2, 3, 5}};
+		int[][] arr2d = {{10, 10, 9, 10, 9, 10}, {7, 6, 4, 8, 8, 5}, {8, 9, 7, 8, 8, 7}, {0, 0, 2, 3, 0, 2}};
 		int[] arrTotal = (classTotal(arr2d));
 		System.out.println(Arrays.deepToString(arr2d));
 		System.out.println(Arrays.toString(arrTotal));
+		System.out.println(numPassing(arr2d));
 	}
 	public static int gradeTotal (int[] grades) {
 		int sumGrades = 0;
@@ -23,5 +24,16 @@ public class GradeBook {
 			totals[i] = gradeTotal(classroom[i]);
 		}
 		return totals;
+	}
+	
+	public static int numPassing(int [][] classroom) {
+		int passed = 0;
+		int[] studentScores = classTotal(classroom);
+		for (int i = 0; i < studentScores.length; i++) {
+			if (studentScores[i] >= 30) {
+				passed++;
+			}
+		}
+		return passed;
 	}
 }
